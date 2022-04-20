2c69320af9199cc8795333582180101924389021
sam159247 <sam159247@gmail.com>
2022-04-20 22:44:07 +0900

    chore: add config files

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    printWidth: 80,
+    endOfLine: "auto",
+    arrowParens: "avoid",
+    trailingComma: "es5",
+    semi: true,
+    useTabs: false,
+    singleQuote: false,
+    bracketSpacing: true,
+};
